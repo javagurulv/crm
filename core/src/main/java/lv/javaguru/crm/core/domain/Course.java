3499ad30ea24cf5d5391a37023f1f93093eef886
Igors Mihejevs <mihejevs.igors@gmail.com>
2021-03-18 15:35:35 +0200

    Course added to Domain package, build.gradle refactoring (lombok)

---
+++ core/src/main/java/lv/javaguru/crm/core/domain/Course.java
@@ -0,0 +1,37 @@
+package lv.javaguru.crm.core.domain;
+
+import lombok.Data;
+import javax.persistence.*;
+import java.time.*;
+
+//@Entity
+@Data
+@Table(name = "courses", schema = "javaguru_crm")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "course_type", nullable = false)
+    private String courseType;
+    @Column(name = "start_date", nullable = false)
+    private LocalDate startDate;
+    @Column(name = "end_date", nullable = false)
+    private LocalDate endDate;
+    @Column(name = "day_of_week", nullable = false)
+    private String dayOfWeek;
+    @Column(name = "start_time", nullable = false)
+    private LocalTime startTime;
+    @Column(name = "end_time", nullable = false)
+    private LocalDate endTime;
+    @Column(name = "open_class_date")
+    private LocalDate openClassDate;
+    @Column(name = "open_class_start_time")
+    private LocalTime openClassStartTime;
+    @Column(name = "open_class_end_time")
+    private LocalDate openClassEndTime;
+    @Column(name = "teacher", nullable = false)
+    private String teacher;
+
+}
