c9fb1fe3b28f21197878564858da572b09550b1b
Igors Mihejevs <mihejevs.igors@gmail.com>
2021-03-19 12:15:22 +0200

    Teacher added changelog-*.xml refactoring

--- core/src/main/java/lv/javaguru/crm/core/domain/Teacher.java
+++ core/src/main/java/lv/javaguru/crm/core/domain/Teacher.java
@@ -1,8 +1,23 @@
 package lv.javaguru.crm.core.domain;
 
-import javax.persistence.Entity;
+import lombok.Data;
 
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "teachers", schema = "javaguru_crm")
 public class Teacher {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "second_name", nullable = false)
+    private String secondName;
+    @Column(name = "phone", nullable = false)
+    private String phone;
+    @Column(name = "email", nullable = false)
+    private String email;
 
 }
