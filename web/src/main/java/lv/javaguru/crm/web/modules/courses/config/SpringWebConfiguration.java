a3f5fd3b9f24225526400ed2927544ec12eaee7d
Igors Mihejevs <mihejevs.igors@gmail.com>
2021-03-21 12:15:59 +0200

    Web configuration added

---
+++ web/src/main/java/lv/javaguru/crm/web/modules/courses/config/SpringWebConfiguration.java
@@ -0,0 +1,40 @@
+package lv.javaguru.crm.web.modules.courses.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "lv.javaguru.crm.web.modules.courses")
+public class SpringWebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("classpath:/static/images/");
+        registry.addResourceHandler("/icons/**")
+                .addResourceLocations("classpath:/static/images/icons/");
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver resourceViewResolver = new SpringResourceTemplateResolver();
+        resourceViewResolver.setPrefix("classpath:/templates/");
+        resourceViewResolver.setSuffix(".html");
+        resourceViewResolver.setTemplateMode(TemplateMode.HTML);
+        resourceViewResolver.setCharacterEncoding("UTF-8");
+        resourceViewResolver.setCheckExistence(false);
+        return resourceViewResolver;
+    }
+
+}
---
+++ web/src/main/java/lv/javaguru/crm/web/modules/courses/config/SpringWebMvcInitializer.java
@@ -0,0 +1,32 @@
+package lv.javaguru.crm.web.modules.courses.config;
+
+import lv.javaguru.crm.core.modules.configuration.CourseConfiguration;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+
+public class SpringWebMvcInitializer extends AbstractDispatcherServletInitializer {
+
+    @Override
+    protected WebApplicationContext createRootApplicationContext() {
+        AnnotationConfigWebApplicationContext applicationContext =
+                new AnnotationConfigWebApplicationContext();
+        applicationContext.register(CourseConfiguration.class);
+        return applicationContext;
+    }
+
+    @Override
+    protected WebApplicationContext createServletApplicationContext() {
+        AnnotationConfigWebApplicationContext applicationContext =
+                new AnnotationConfigWebApplicationContext();
+        applicationContext.register(SpringWebConfiguration.class);
+        return applicationContext;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+
+}
