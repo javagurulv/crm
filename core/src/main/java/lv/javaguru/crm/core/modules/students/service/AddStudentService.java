4bba79b8d87656f8e20aae2ba06fd17869c4a5a8
Jaroslav <brutan.jaroslav@gmail.com>
2021-03-28 22:38:28 +0300

    add student service update

--- core/src/main/java/lv/javaguru/crm/core/modules/students/service/AddStudentService.java
+++ core/src/main/java/lv/javaguru/crm/core/modules/students/service/AddStudentService.java
@@ -1,15 +1,34 @@
 package lv.javaguru.crm.core.modules.students.service;
 
 
+import lv.javaguru.crm.core.modules.core_error.CoreError;
 import lv.javaguru.crm.core.modules.students.persistance.JpaStudentRepository;
+import lv.javaguru.crm.core.modules.students.request.AddStudentRequest;
+import lv.javaguru.crm.core.modules.students.response.AddStudentResponse;
+import lv.javaguru.crm.core.modules.students.validators.AddStudentValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
 public class AddStudentService {
 
     @Autowired
-    private JpaStudentRepository studentRepository;
+    private JpaStudentRepository repository;
+    @Autowired
+    private AddStudentValidator validator;
+
+    public AddStudentResponse execute (AddStudentRequest request){
+
+        List<CoreError> errors = validator.validate(request);
+        if (!errors.isEmpty()){
+            return new AddStudentResponse(errors);
+        }
+
+        repository.save(request.getStudent());
+        return new AddStudentResponse(request.getStudent());
+    }
 }
