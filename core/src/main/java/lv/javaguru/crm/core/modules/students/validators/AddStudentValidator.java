d28ea956e7801c8ded1b0a57ec6bfd585c1a5439
Jaroslav <brutan.jaroslav@gmail.com>
2021-03-28 22:39:55 +0300

    add student validator update

--- core/src/main/java/lv/javaguru/crm/core/modules/students/validators/AddStudentValidator.java
+++ core/src/main/java/lv/javaguru/crm/core/modules/students/validators/AddStudentValidator.java
@@ -10,10 +10,10 @@ import java.util.regex.Pattern;
 
 public class AddStudentValidator {
 
-    private static final Pattern PATTERN_EMAIL = Pattern.compile("^([a-z0-9_\\.-]+)@([a-z0-9_\\.-]+)\\.([a-z\\.]{2,6})$", Pattern.CASE_INSENSITIVE);
+   /* private static final Pattern PATTERN_EMAIL = Pattern.compile("^([a-z0-9_\\.-]+)@([a-z0-9_\\.-]+)\\.([a-z\\.]{2,6})$", Pattern.CASE_INSENSITIVE);
     private static final Pattern PATTERN_PHONE_LV = Pattern.compile("^\\++\\d{8,11}$");
     private static final Pattern PATTERN_PASSWORD = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$");
-
+*/
     public List<CoreError> validate (AddStudentRequest request) {
         List<CoreError> errors = new ArrayList<>();
 
