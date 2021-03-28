5e631b80ba40d926302420ebeb3924406322ec9a
Jaroslav <brutan.jaroslav@gmail.com>
2021-03-28 22:38:17 +0300

    add student response update

--- core/src/main/java/lv/javaguru/crm/core/modules/students/response/AddStudentResponse.java
+++ core/src/main/java/lv/javaguru/crm/core/modules/students/response/AddStudentResponse.java
@@ -1,16 +1,21 @@
 package lv.javaguru.crm.core.modules.students.response;
 
 
+import lombok.Getter;
 import lv.javaguru.crm.core.modules.core_error.CoreError;
 import lv.javaguru.crm.core.modules.core_error.CoreResponse;
+import lv.javaguru.crm.core.modules.students.domain.Student;
 
 import java.util.List;
 
 public class AddStudentResponse extends CoreResponse {
 
+    @Getter
+    private Student student;
+
     public AddStudentResponse(List<CoreError> coreError){
         super(coreError);
     }
 
-
+    public AddStudentResponse(Student student){this.student = student;}
 }
