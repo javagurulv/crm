5bea5537670eb456eac6f1e1da0437b8d600cdf3
Jaroslav <brutan.jaroslav@gmail.com>
2021-03-28 22:38:05 +0300

    add student request update

--- core/src/main/java/lv/javaguru/crm/core/modules/students/request/AddStudentRequest.java
+++ core/src/main/java/lv/javaguru/crm/core/modules/students/request/AddStudentRequest.java
@@ -2,11 +2,13 @@ package lv.javaguru.crm.core.modules.students.request;
 
 
 import lombok.Getter;
+import lombok.Setter;
 import lv.javaguru.crm.core.modules.students.domain.Student;
 
 public class AddStudentRequest {
 
     @Getter
+    @Setter
     private Student student;
 
     public AddStudentRequest(Student student) {
