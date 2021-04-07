8211feb31f27f9eba88db3bc6d84f8adeb44acaa
tipka <tipkanet@gmail.com>
2021-04-07 18:35:45 +0300

    add GetStudentListByCriteria request & validator

--- core/src/main/java/lv/javaguru/crm/core/modules/students/service/GetStudentListByCriteriaService.java
+++ core/src/main/java/lv/javaguru/crm/core/modules/students/service/GetStudentListByCriteriaService.java
@@ -3,10 +3,9 @@ package lv.javaguru.crm.core.modules.students.service;
 import lv.javaguru.crm.core.modules.core_error.CoreError;
 import lv.javaguru.crm.core.modules.students.domain.Student;
 import lv.javaguru.crm.core.modules.students.persistance.JpaStudentRepository;
-import lv.javaguru.crm.core.modules.students.request.GetStudentListRequest;
+import lv.javaguru.crm.core.modules.students.request.GetStudentListByCriteriaRequest;
 import lv.javaguru.crm.core.modules.students.response.GetStudentListResponse;
 import lv.javaguru.crm.core.modules.students.validators.GetStudentListByCriteriaValidator;
-import lv.javaguru.crm.core.modules.students.validators.GetStudentListValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,33 +23,21 @@ public class GetStudentListByCriteriaService {
     @Autowired
     private GetStudentListByCriteriaValidator validator;
 
-    public GetStudentListResponse execute(GetStudentListRequest request) {
+    public GetStudentListResponse execute(GetStudentListByCriteriaRequest request) {
+
         List<CoreError> errors = validator.validate(request);
 
-        if (studentRepository.findAll().isEmpty()) {
-            errors.add(new CoreError("database", "Database is empty"));
-            return new GetStudentListResponse(errors, new ArrayList<>());
-        }
-        List<Student> students = studentRepository.findAll();
-        return new GetStudentListResponse(students);
-    }
-}
-
-// Iterable<T> findAll(Predicate); criteriaBuilder.or()
-
-    /*  List<CoreError> errors = validator.validate(request);
         if (!errors.isEmpty()) {
             return new GetStudentListResponse(errors, null);
         }
 
-        List<Student> listStudents = studentRepository.findByNameAndSurnameAndPhoneNumberAndComment(
-                "%" + request.getQueryString() + "%",
-                "%" + request.getQueryString() + "%",
-                "%" + request.getQueryString() + "%",
-                "%" + request.getQueryString() + "%"
-        );
+        List<Student> students = studentRepository.findAll(request.getStudentListRequest());
 
-        return new GetStudentListResponse(null, listStudents);
+        if (students.isEmpty()) {
+            errors.add(new CoreError("database", "There is no data for this criteria."));
+            return new GetStudentListResponse(errors, null);
+        }
+
+        return new GetStudentListResponse(null, students);
     }
 }
-*/
--- core/src/main/java/lv/javaguru/crm/core/modules/students/validators/GetStudentListByCriteriaValidator.java
+++ core/src/main/java/lv/javaguru/crm/core/modules/students/validators/GetStudentListByCriteriaValidator.java
@@ -1,16 +1,31 @@
 package lv.javaguru.crm.core.modules.students.validators;
 
 import lv.javaguru.crm.core.modules.core_error.CoreError;
-import lv.javaguru.crm.core.modules.students.request.GetStudentListRequest;
+import lv.javaguru.crm.core.modules.students.request.GetStudentListByCriteriaRequest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class GetStudentListByCriteriaValidator {
 
-    public List<CoreError> validate (GetStudentListRequest getStudentListRequest){
+    private static final Pattern PATTERN_ALPHABET = Pattern.compile("[a-zA-Z]+");
 
-        return new ArrayList<>();
+    public List<CoreError> validate (GetStudentListByCriteriaRequest studentListRequest){
+
+        List<CoreError> errors = new ArrayList<>();
+
+        if (studentListRequest.getStudentListRequest().isBlank() || studentListRequest.getStudentListRequest() == null) {
+            CoreError error = new CoreError("searchCriteria", "It is required to define at list one of the search criteria.");
+            errors.add(error);
+        }
+
+        if (!PATTERN_ALPHABET.matcher(studentListRequest.getStudentListRequest()).matches()) {
+            CoreError error = new CoreError("searchCriteria", "The search criteria contains incorrect symbols.");
+            errors.add(error);
+        }
+
+        return errors;
 
     }
 }
